2f8890075f086e88ccd0c89f0da2efbf9a389869
Victor Porton <porton.victor@gmail.com>
2023-02-20 17:01:22 +0200

    decode_token_amount

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,6 @@
 use std::array::TryFromSliceError;
-use std::convert::Infallible;
-use std::iter::{once, repeat};
+use std::cmp::max;
+use std::iter::repeat;
 use std::num::ParseIntError;
 use hex::{decode, FromHexError};
 use derive_more::From;
@@ -33,25 +33,38 @@ pub fn decode_xrp_amount(s: &str) -> Result<u64, ParseIntError> {
     s.parse::<u64>()
 }
 
+const TOKEN_DIGITS: usize = 6;
+
 pub fn encode_token_amount(amount: u64) -> String {
     let mut s = amount.to_string();
-    const DIGITS: usize = 6;
-    if s.len() < DIGITS + 1 { // at least one digit before the dot
-        s = repeat("0").take(DIGITS + 1 - s.len()).chain(once(s.as_str()))
-            .flat_map(|s| s.chars()).collect();
-    }
-    // while s.len() < DIGITS + 1 { // at least one digit before the dot
+    // The following is presumably a little slow...
+    // while s.len() < TOKEN_DIGITS + 1 { // at least one digit before the dot
     //     s = ["0", &s].concat();
     // }
-    s.insert(s.len() - DIGITS, '.');
+    // ... so iterator magic
+    if s.len() < TOKEN_DIGITS + 1 { // at least one digit before the dot
+        s = repeat('0').take(TOKEN_DIGITS + 1 - s.len()).chain(s.chars()).collect();
+    }
+    s.insert(s.len() - TOKEN_DIGITS, '.');
     s
         .trim_matches(&['0'] as &[_])
-        .trim_matches(&['.'] as &[_]).to_owned()
-    // FIXME: Remove leading zeros.
+        .trim_end_matches(&['.'] as &[_]).to_owned()
 }
 
-/// FIXME: Scientific notation
+/// FIXME: Support scientific notation
 pub fn decode_token_amount(s: &str) -> Result<u64, ParseIntError> {
-    // FIXME
-    s.parse::<u64>()
+    if let Some(dot_pos) = s.chars().position(|c| c== '.') {
+        let mut s = s.to_owned();
+        let digits_after_dot = s.len() - dot_pos;
+        if digits_after_dot < TOKEN_DIGITS {
+            s = s.chars().chain(repeat('0').take(TOKEN_DIGITS - digits_after_dot)).collect();
+        }
+        s.remove(dot_pos);
+        s = s[.. max(digits_after_dot, TOKEN_DIGITS) - TOKEN_DIGITS].to_owned();
+        s.parse::<u64>()
+    } else {
+        s.parse::<u64>()
+    }
 }
+
+// TODO: Unit tests.
