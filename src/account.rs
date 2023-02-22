1178d6cfa7f1703dcdaebda01242b6f5d63af1ec
Victor Porton <porton.victor@gmail.com>
2023-02-23 00:10:15 +0200

    bug fixes

--- src/account.rs
+++ src/account.rs
@@ -88,8 +88,8 @@ impl PaginatorExtractor for ChannelPaginator {
     }
 }
 
-pub async fn account_channels<'a, A>(api: &A, request: &'a TypedRequest<'a, ChannelsRequest>)
-                           -> Result<(TypedResponse<ChannelResponse>, ChannelPaginator), A::Error>
+pub async fn account_channels<'a, A>(api: &'a A, request: &'a TypedRequest<'a, ChannelsRequest>)
+                           -> Result<(TypedResponse<ChannelResponse>, Paginator<'a, A, ChannelPaginator>), A::Error>
     where A: Api,
           A::Error: From<ParseResponseError> + From<WrongFieldsError>
 {
