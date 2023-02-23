f5f4bd170b40a57a898cd4d945ba1f4752ceb113
Victor Porton <porton.victor@gmail.com>
2023-02-23 18:44:31 +0200

    tried not working Api trait method

--- src/connection.rs
+++ src/connection.rs
@@ -31,6 +31,9 @@ impl XrpError {
 pub trait Api {
     type Error;
     async fn call<'a>(&self, request: Request<'a>) -> Result<Response, Self::Error>;
+    // async fn call_typed<'a, T: Into<Request<'a>>, U: TryFrom<Response>>(&self, request: T) -> Result<U, Self::Error> {
+    //     Ok(self.call(request.into()).await?.try_into().map_err(|_| WrongFieldsError::new())?)
+    // }
 }
 
 pub struct JsonRpcApi {
