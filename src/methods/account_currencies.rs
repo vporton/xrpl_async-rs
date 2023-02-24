638885fceda92b2d909b9033d1c06f746ac0d821
Victor Porton <porton.victor@gmail.com>
2023-02-24 04:43:59 +0200

    account_currencies method

--- src/methods/account_currencies.rs
+++ src/methods/account_currencies.rs
@@ -1,27 +1,16 @@
-use std::convert::{From, Into};
-use serde_json::{Map, Value};
+use std::convert::From;
+use serde::{Deserialize, Serialize};
 use crate::address::Address;
 use crate::connection::Api;
 use crate::types::{Hash, Ledger};
-use crate::json::ValueExt;
-use crate::request::{FormatParams, TypedRequest};
-use crate::response::{ParseResponse, ParseResponseError, TypedResponse, WrongFieldsError};
+use crate::request::TypedRequest;
+use crate::response::{ParseResponseError, TypedResponse, WrongFieldsError};
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct CurrenciesRequest {
     pub account: Address,
+    #[serde(flatten)]
     pub ledger: Ledger,
-}
-
-impl FormatParams for &CurrenciesRequest {
-    fn to_json(&self) -> Map<String, Value> {
-        let mut j = Map::new();
-        // TODO: Move to `lazy_static`.
-        j.insert("account".to_owned(), Value::String(self.account.encode()));
-        let (ledger_key, ledger_value) = self.ledger.to_json();
-        j.insert(ledger_key.to_owned(), ledger_value);
-        j
-    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -31,22 +20,6 @@ pub struct CurrenciesResponse {
     pub receive_currencies: Vec<String>,
     pub send_currencies: Vec<String>,
     pub validated: Option<bool>,
-}
-
-impl ParseResponse for CurrenciesResponse {
-    fn from_json(value: &Value) -> Result<Self, ParseResponseError> {
-        Ok(CurrenciesResponse {
-            ledger_hash: value.get("ledger_hash").map(|v| Ok::<_, WrongFieldsError>(v.as_hash_valid()?)).transpose()?,
-            ledger_index: value.get("ledger_index").map(|v| Ok::<_, WrongFieldsError>(v.as_u32_valid()?)).transpose()?,
-            receive_currencies: value.get_valid("receive_currencies")?.as_array_valid()?
-                .into_iter().map(|v| -> Result<_, WrongFieldsError> { Ok(v.as_str_valid()?.to_owned()) })
-                .collect::<Result<Vec<String>, WrongFieldsError>>()?,
-            send_currencies: value.get_valid("send_currencies")?.as_array_valid()?
-                .into_iter().map(|v| -> Result<_, WrongFieldsError> { Ok(v.as_str_valid()?.to_owned()) })
-                .collect::<Result<Vec<String>, WrongFieldsError>>()?,
-            validated: value.get("validated").map(|v| Ok::<_, WrongFieldsError>(v.as_bool_valid()?)).transpose()?,
-        })
-    }
 }
 
 pub async fn account_currencies<'a, A>(api: &'a A, data: &'a CurrenciesRequest)
@@ -59,5 +32,5 @@ pub async fn account_currencies<'a, A>(api: &'a A, data: &'a CurrenciesRequest)
         api_version: Some(1),
         data,
     };
-    Ok(api.call((&request).into()).await?.try_into()?)
+    Ok(api.call((&request).try_into().map_err(|_| WrongFieldsError::new())?).await?.try_into()?)
 }
