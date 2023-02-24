1156fc8167f14a465c94ce1e432e965a66397cc5
Victor Porton <porton.victor@gmail.com>
2023-02-25 00:33:40 +0200

    AccountRoot deserializer finished

--- src/objects/account_root.rs
+++ src/objects/account_root.rs
@@ -4,6 +4,7 @@ use crate::types::Hash;
 use crate::types::xrp::deserialize;
 
 pub struct Flags(u32);
+// TODO: Values of flags: https://xrpl.org/accountroot.html
 
 impl<'de> Deserialize<'de> for Flags {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
@@ -21,7 +22,7 @@ pub struct AccountRoot {
     #[serde(with = "crate::types::option_xrp")]
     pub balance: Option<u64>,
     #[serde(rename = "BurnedNFTokens")]
-    pub burned_nft_tokens: Option<u32>,
+    pub burned_nf_tokens: Option<u32>,
     #[serde(rename = "Domain")]
     pub domain: Option<String>,
     #[serde(rename = "EmailHash")]
@@ -30,5 +31,26 @@ pub struct AccountRoot {
     pub flags: Flags,
     #[serde(rename = "MessageKey")]
     pub message_key: Option<AccountPublicKey>,
-    // FIXME: more fields
+    #[serde(rename = "MintedNFTokens")]
+    pub minted_nf_tokens: Option<u32>,
+    #[serde(rename = "NFTokenMinter")]
+    pub nf_token_minter: Option<Address>,
+    #[serde(rename = "OwnerCount")]
+    pub owner_count: u32,
+    #[serde(rename = "PreviousTxnID")]
+    pub previous_txn_id: Hash<32>,
+    #[serde(rename = "PreviousTxnLgrSeq")]
+    pub previous_txn_lgr_seq: u32,
+    #[serde(rename = "RegularKey")]
+    pub regular_key: Option<Address>,
+    #[serde(rename = "Sequence")]
+    pub sequence: u32,
+    #[serde(rename = "TicketCount")]
+    pub ticket_count: Option<u8>, // TODO: No need for `Option` here.
+    #[serde(rename = "TickSize")]
+    pub tick_size: Option<u8>,
+    #[serde(rename = "TransferRate")]
+    pub transfer_rate: Option<u32>,
+    #[serde(rename = "WalletLocator")]
+    pub wallet_locator: Option<Hash<32>>,
 }
