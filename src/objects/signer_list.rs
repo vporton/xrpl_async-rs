0a68c8360627c740ee41de89db5105ed6ed7eb63
Victor Porton <porton.victor@gmail.com>
2023-02-25 02:05:36 +0200

    SignerListFlags

--- src/objects/signer_list.rs
+++ src/objects/signer_list.rs
@@ -1,4 +1,13 @@
-use serde::Deserialize;
+use serde::{de, Deserialize, Deserializer};
+
+pub struct SignerListFlags(u32);
+// TODO: Values of flags: https://xrpl.org/signerlist.html#signerlist-flags
+
+impl<'de> Deserialize<'de> for SignerListFlags {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+        Ok(SignerListFlags(u32::deserialize(deserializer)?.try_into().map_err(de::Error::custom)?))
+    }
+}
 
 #[derive(Debug, Deserialize)]
 pub struct SignerList {
