d055b4f87f9a6941848822c3d5620848e64886df
Victor Porton <porton.victor@gmail.com>
2023-02-25 02:14:47 +0200

    SignerList implemented

--- src/objects/signer_list.rs
+++ src/objects/signer_list.rs
@@ -1,5 +1,8 @@
 use serde::{de, Deserialize, Deserializer};
+use crate::address::Address;
+use crate::types::Hash;
 
+#[derive(Debug)]
 pub struct SignerListFlags(u32);
 // TODO: Values of flags: https://xrpl.org/signerlist.html#signerlist-flags
 
@@ -10,6 +13,29 @@ impl<'de> Deserialize<'de> for SignerListFlags {
 }
 
 #[derive(Debug, Deserialize)]
+pub struct SignerEntry {
+    #[serde(rename = "Account")]
+    pub account: Address,
+    #[serde(rename = "SignerWeight")]
+    pub signer_weight: u16,
+    #[serde(rename = "WalletLocator")]
+    pub wallet_locator: Hash<32>,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct SignerList {
-    // FIXME: more fields
+    #[serde(rename = "Flags")]
+    pub flags: SignerListFlags,
+    #[serde(rename = "OwnerNode")]
+    pub owner_node: u64,
+    #[serde(rename = "PreviousTxnID")]
+    pub previous_txn_id: Hash<32>,
+    #[serde(rename = "PreviousTxnLgrSeq")]
+    pub previous_txn_lgr_seq: u32,
+    #[serde(rename = "SignerEntries")]
+    pub signer_entries: Vec<SignerEntry>,
+    #[serde(rename = "SignerListID")]
+    pub signer_list_id: u32,
+    #[serde(rename = "SignerQuorum")]
+    pub signer_quorum: u32,
 }
