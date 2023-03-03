3ef0acf8f4d488a943eb5f1ca419798fcce3d37e
Victor Porton <porton.victor@gmail.com>
2023-03-04 01:45:11 +0200

    removed a useless (for now) source file

--- src/txs/account_set.rs
+++
@@ -1,9 +0,0 @@
-use serde::Serialize;
-use crate::address::Address;
-
-#[derive(Serialize)]
-pub struct AccountSet {
-    #[serde(rename = "Account")]
-    pub account: Address,
-    // FIXME: more fields
-}
